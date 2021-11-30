a85db6a2f79759b14935d022f67e1ff1acb69799
Nikita Tsarkov <tsarkov90@gmail.com>
2021-11-30 16:10:29 +0300

    надо доделать 3 задание, проверить его

--- HW_1.2.a.sql
+++ HW_1.2.a.sql
@@ -1,10 +1,10 @@
 -- DISTINCT, WHERE, ORDER BY
--- Ценные бумаги по которым совершили сделки на покупку за первый час
+-- Ценные бумаги по которым кидали заявки на покупку за первый час
 SELECT DISTINCT security_code
 FROM stock_orders
 WHERE order_time >= '10:00' AND order_time < '11:00'
     AND buysell = 'B'
-    AND action = 2
+    AND action = 1
 ORDER BY security_code desc
 
 
